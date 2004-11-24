a39e7311cbc964b175efd1276fe8e428bab5523d
Arch Librarian <arch@canonical.com>
2004-11-24 10:00:31 +0000

    Add new examples Author: mdz Date: 2002-08-27 23:20:42 GMT Add new examples

---
+++ doc/examples/checkstate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import apt_pkg
+apt_pkg.init()
+
+cache = apt_pkg.GetCache()
+packages = cache.Packages
+
+uninstalled, updated, upgradable = {}, {}, {}
+
+for package in packages:
+	versions = package.VersionList
+	if not versions:
+		continue
+	version = versions[0]
+	for other_version in versions:
+		if apt_pkg.VersionCompare(version.VerStr, other_version.VerStr)<0:
+			version = other_version
+	if package.CurrentVer:
+		current = package.CurrentVer
+		if apt_pkg.VersionCompare(current.VerStr, version.VerStr)<0:
+			upgradable[package.Name] = version
+			break
+		else:
+			updated[package.Name] = current
+	else:
+		uninstalled[package.Name] = version
+
+
+for l in (uninstalled, updated, upgradable):
+	print l.items()[0]
