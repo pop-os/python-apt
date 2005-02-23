5a4c9edb5772d54a68ca043f4b4ebd85a871bd5d
Michael Vogt <michael.vogt@ubuntu.com>
2005-02-23 21:03:13 +0000

    * example for depcache and progress callback commited

--- doc/examples/depcache.py
+++ doc/examples/depcache.py
@@ -3,19 +3,33 @@
 
 import apt_pkg
 
+class TextProgress:
+    def __init__(self):
+        self.last = 1
+    def show(self,Percent):
+        if self.last < Percent:
+            print "\r%.2f            "  % Percent,
+            self.last = Percent+0.1
+        if Percent >= 100:
+            print "\r100.0         "
+            self.last = 0
+
 def Update(Percent, data):
-    pass
+    # data is a TextProgress class
+    progress = data           
+    progress.show(Percent)
+
 
 apt_pkg.init()
-cache = apt_pkg.GetCache()
+
+progress = TextProgress()
+cache = apt_pkg.GetCache(Update, progress)
 print cache.PackageCount
 
 iter = cache["base-config"]
 print iter
 
-depcache = apt_pkg.GetDepCache(cache)
-depcache.SetProgressCallback(Update, None)
-depcache.Init()
+depcache = apt_pkg.GetDepCache(cache, Update, progress)
 print depcache
 print depcache.InstCount
 
