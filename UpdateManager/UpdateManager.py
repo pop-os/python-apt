65b8d2037f671cff8d3903d73fe94ef06e682a7b
Sebastian Heinlein <sebastian.heinlein@web.de>
2006-04-19 00:19:12 +0200

    * remove all uses of update-manager.png in the glade files * always show the download size of the currently selected updates   fix fo bug #39579

--- UpdateManager/UpdateManager.py
+++ UpdateManager/UpdateManager.py
@@ -411,6 +411,8 @@ class UpdateManager(SimpleGladeApp):
     if name in self.packages:
       self.packages.remove(name)
       self.dl_size -= pkg.packageSize
+      self.label_downsize.set_markup(_("Download size: %s" % \
+                                     apt_pkg.SizeToStr(self.dl_size)))
       if len(self.packages) == 0:
         self.button_install.set_sensitive(False)
 
@@ -419,6 +421,8 @@ class UpdateManager(SimpleGladeApp):
     if name not in self.packages:
       self.packages.append(name)
       self.dl_size += pkg.packageSize
+      self.label_downsize.set_markup(_("Download size: %s" % \
+                                     apt_pkg.SizeToStr(self.dl_size)))
       if len(self.packages) > 0:
         self.button_install.set_sensitive(True)
 
