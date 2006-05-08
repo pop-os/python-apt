f4fd803ae9d2a0bbdb03f5b9fc21e432ebbdb0b3
Michael Vogt <michael.vogt@ubuntu.com>
2006-05-08 16:38:33 +0200

    * merged r192 from mainline

--- setup.py
+++ setup.py
@@ -7,22 +7,18 @@ import string, glob
 
 
 # The apt_pkg module
-files = string.split(parse_makefile("python/makefile")["APT_PKG_SRC"]);
-for i in range(0,len(files)):
-    files[i] = "python/"+ files[i];
-apt_pkg = Extension("apt_pkg", files,
-		libraries=["apt-pkg"]);
+files = map(lambda source: "python/"+source,
+            string.split(parse_makefile("python/makefile")["APT_PKG_SRC"]))
+apt_pkg = Extension("apt_pkg", files, libraries=["apt-pkg"]);
 
 # The apt_inst module
-files = string.split(parse_makefile("python/makefile")["APT_INST_SRC"]);
-for i in range(0,len(files)):
-    files[i] = "python/"+ files[i];
-apt_inst = Extension("apt_inst", files,
-		libraries=["apt-pkg","apt-inst"]);
+files = map(lambda source: "python/"+source,
+            string.split(parse_makefile("python/makefile")["APT_INST_SRC"]))
+apt_inst = Extension("apt_inst", files, libraries=["apt-pkg","apt-inst"]);
 
 
 setup(name="python-apt", 
-      version="0.6.13",
+      version="0.6.17",
       description="Python bindings for APT",
       author="APT Development Team",
       author_email="deity@lists.debian.org",
