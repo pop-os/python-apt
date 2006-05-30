d5c37fe74409401dd6cf1b0a37a789083a0cc413
Michael Vogt <michael.vogt@ubuntu.com>
2006-05-30 13:08:38 +0200

    * as suggested by mdz use a wrapper script instead of   re-exec

--- DistUpgrade/dist-upgrade.py
+++ DistUpgrade/dist-upgrade.py
@@ -4,30 +4,15 @@ from DistUpgradeControler import DistUpgradeControler
 from DistUpgradeConfigParser import DistUpgradeConfig
 import logging
 import os
-import os.path
 import sys
 
 if __name__ == "__main__":
 
-    # init logging
     logging.basicConfig(level=logging.DEBUG,
                         filename="/var/log/dist-upgrade.log",
                         format='%(asctime)s %(levelname)s %(message)s',
                         filemode='w')
 
-    # make sure we run under a segv-handler
-    if not os.environ.has_key("LD_PRELOAD") or \
-           not "libSegFault" in os.environ["LD_PRELOAD"]:
-        fd = os.open("/var/log/dist-upgrade-segv.log",
-                     os.O_RDWR|os.O_CREAT|os.O_TRUNC)
-        os.dup2(fd,1)
-        os.dup2(fd,2)
-        # restart ourself
-        os.execl("/usr/bin/catchsegv", "catchsegv", sys.argv[0])
-    else:
-        logging.debug("Runing with segv-handler: %s", os.environ["LD_PRELOAD"])
-
-    # init config and get a view
     config = DistUpgradeConfig()
     requested_view= config.get("View","View")
     try:
