444b2afea3b58cc55fbf4b9deb6d20c4475c8fef
Julian Andres Klode <jak@debian.org>
2009-04-14 20:27:49 +0200

    * python/pkgmanager.cc: Use tp_methods and tp_getset for PkgManagerType

--- python/apt_pkgmodule.cc
+++ python/apt_pkgmodule.cc
@@ -487,6 +487,7 @@ extern "C" void initapt_pkg()
    if (PyType_Ready(&PkgDepCacheType) == -1) return;
    if (PyType_Ready(&PkgAcquireType) == -1) return;
    if (PyType_Ready(&PackageIndexFileType) == -1) return;
+   if (PyType_Ready(&PkgManagerType) == -1) return;
 
 
    // Initialize the module
