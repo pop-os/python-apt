a84352261cd34aaa34b3c4507d9d8e6216170050
Michael Vogt <michael.vogt@ubuntu.com>
2011-12-08 20:31:15 +0100

    * tests/test_apt_cache.py:   - add additional check if provides test can actually be run

--- tests/test_apt_cache.py
+++ tests/test_apt_cache.py
@@ -74,6 +74,9 @@ class TestAptCache(unittest.TestCase):
         apt.apt_pkg.config.set("Apt::architecture", "i386")
         cache = apt.Cache(rootdir="./data/test-provides/")
         cache.open()
+        if len(cache) == 0:
+            logging.warn("skipping test_get_provided_packages, cache empty?!?")
+            return
         # a true virtual pkg
         l = cache.get_providing_packages("mail-transport-agent")
         self.assertTrue(len(l) > 0)
@@ -84,6 +87,9 @@ class TestAptCache(unittest.TestCase):
         apt.apt_pkg.config.set("Apt::architecture", "i386")
         # create highlevel cache and get the lowlevel one from it
         highlevel_cache = apt.Cache(rootdir="./data/test-provides")
+        if len(highlevel_cache) == 0:
+            logging.warn("skipping test_log_level_pkg_provides, cache empty?!?")
+            return
         # low level cache provides list of the pkg
         cache = highlevel_cache._cache
         l = cache["mail-transport-agent"].provides_list
