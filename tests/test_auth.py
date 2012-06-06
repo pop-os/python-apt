6f8b10fe939609a1afc8de057813e1d43bc47558
Sebastian Heinlein <devel@glatzor.de>
2012-06-06 12:53:44 +0200

    Add a fakeroot wrapper for apt-key

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -119,12 +119,14 @@ class TestAuthKeys(unittest.TestCase):
         self.tmpdir = tempfile.mkdtemp()
         self.addCleanup(shutil.rmtree, self.tmpdir)
         apt_pkg.config.set("Dir", self.tmpdir)
+        apt_pkg.config.set("Dir::Bin::Apt-key", "fakeroot-apt-key")
         apt_pkg.config.set("Dir::Etc", "etc/apt/")
         trustedparts_dir = apt_pkg.config.find_dir("Dir::Etc::Trustedparts")
         confparts_dir = apt_pkg.config.find_dir("Dir::Etc::parts")
         self.assertTrue(trustedparts_dir.startswith(self.tmpdir))
         os.makedirs(trustedparts_dir)
         os.makedirs(confparts_dir)
+        shutil.copy("fakeroot-apt-key", self.tmpdir)
 
     def _restore_apt_config(self, cnf):
         """Restore previous apt configuration."""
