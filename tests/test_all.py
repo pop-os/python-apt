7cf77010c108df676d3e277716f790b994bba28e
Michael Vogt <egon@debian-devbox>
2013-05-08 18:22:28 +0200

    add py2.6 compatibility

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,8 +6,15 @@
 # notice and this notice are preserved.
 """Run all available unit tests."""
 import os
-import unittest
 import sys
+
+try:
+    import unittest.runner
+    import unittest
+except ImportError:
+    # py2.6 compat
+    import unittest2 as unittest
+
 
 # workaround for py3.2 that apparently does not have this anymore
 # it has "abiflags" 
