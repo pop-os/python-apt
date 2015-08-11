7f34874d812bec5ba5d97251257a608532075aeb
Julian Andres Klode <jak@debian.org>
2015-08-11 14:26:14 +0200

    test_paths: Drop the indexrecord test

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -110,19 +110,6 @@ class TestPath(unittest.TestCase):
                 index.archive_uri(self.file_bytes)
                 index.archive_uri(self.file_unicode)
 
-    def test_index_records(self):
-        index = apt_pkg.IndexRecords()
-        index.load(u"./data/misc/foo_Release")
-        index.load(b"./data/misc/foo_Release")
-
-        hash1, size1 = index.lookup(u"main/i18n/Index")
-        hash2, size2 = index.lookup(b"main/i18n/Index")
-
-        self.assertEqual(size1, size2)
-        self.assertEqual(str(hash1), str(hash2))
-        self.assertEqual(str(hash1), ("SHA256:fefed230e286d832ab6eb0fb7b72"
-                                      "442165b50df23a68402ae6e9d265a31920a2"))
-
     def test_lock(self):
         apt_pkg.get_lock(self.file_unicode, True)
         apt_pkg.get_lock(self.file_bytes, True)
