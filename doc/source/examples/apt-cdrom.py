c37673afae383b380c0ea2fb454051309d25a090
Martin Pitt <martin.pitt@ubuntu.com>
2015-10-27 10:48:36 +0100

    doc/source/examples/apt-cdrom.py: Fix PEP-8 errors.

--- doc/source/examples/apt-cdrom.py
+++ doc/source/examples/apt-cdrom.py
@@ -6,33 +6,33 @@ import apt
 
 
 def show_help():
-    print ("apt %s compiled on %s %s" % (apt_pkg.VERSION,
-                    apt_pkg.DATE, apt_pkg.TIME))
+    print("apt %s compiled on %s %s" % (apt_pkg.VERSION,
+                                        apt_pkg.DATE, apt_pkg.TIME))
     if apt_pkg.config.find_b("version"):
         return 0
 
     # Copied from apt-cdrom
-    print ("Usage: apt-cdrom [options] command\n"
-           "\n"
-           "apt-cdrom is a tool to add CDROM's to APT's source list. The\n"
-           "CDROM mount point and device information is taken from apt.conf\n"
-           "and /etc/fstab.\n"
-           "\n"
-           "Commands:\n"
-           "   add - Add a CDROM\n"
-           "   ident - Report the identity of a CDROM\n"
-           "\n"
-           "Options:\n"
-           "  -h   This help text\n"
-           "  -d   CD-ROM mount point\n"
-           "  -r   Rename a recognized CD-ROM\n"
-           "  -m   No mounting\n"
-           "  -f   Fast mode, don't check package files\n"
-           "  -a   Thorough scan mode\n"
-           "  -c=? Read this configuration file\n"
-           "  -o=? Set an arbitrary configuration option, eg -o "
-           "dir::cache=/tmp\n"
-           "See fstab(5)")
+    print("Usage: apt-cdrom [options] command\n"
+          "\n"
+          "apt-cdrom is a tool to add CDROM's to APT's source list. The\n"
+          "CDROM mount point and device information is taken from apt.conf\n"
+          "and /etc/fstab.\n"
+          "\n"
+          "Commands:\n"
+          "   add - Add a CDROM\n"
+          "   ident - Report the identity of a CDROM\n"
+          "\n"
+          "Options:\n"
+          "  -h   This help text\n"
+          "  -d   CD-ROM mount point\n"
+          "  -r   Rename a recognized CD-ROM\n"
+          "  -m   No mounting\n"
+          "  -f   Fast mode, don't check package files\n"
+          "  -a   Thorough scan mode\n"
+          "  -c=? Read this configuration file\n"
+          "  -o=? Set an arbitrary configuration option, eg -o "
+          "dir::cache=/tmp\n"
+          "See fstab(5)")
     return 0
 
 
