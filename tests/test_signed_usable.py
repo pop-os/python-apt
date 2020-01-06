3b4d76bb40febb66e076695f026f030834226810
Julian Andres Klode <jak@debian.org>
2020-01-06 17:25:16 +0100

    apt/cache.py: Check for unauthenticated in fetch_archives/commit
    This follows the same behavior as for fetch_binary() /
    fetch_source(), it is a follow-up to
    
    CVE-2019-15796
    LP: #1858973

--- tests/test_signed_usable.py
+++ tests/test_signed_usable.py
@@ -19,6 +19,20 @@ import apt.progress.text
 
 import testcommon
 
+# A change in APT to early fail acquire items with weak hashes caused it
+# to call progress methods before calling Start(), which confused python-apt's
+# locking handling. Hence we need to skip such tests for now, until we can test
+# against an apt version with the fix:
+#
+#     https://salsa.debian.org/apt-team/apt/commit/84176f6c
+#
+RUN_CRASHING_TESTS = False
+
+# Message APT gives us when hashes are too weak
+CACHE_MSG_WEAK_HASH = (
+    "Insufficient information available to perform this download securely"
+)
+
 
 class TestSignedUsable(testcommon.TestCase):
     """Test fetch_binary() and fetch_source() signature checking."""
@@ -54,6 +68,11 @@ class TestSignedUsable(testcommon.TestCase):
         self.progress = apt.progress.text.AcquireProgress
         apt.progress.text.AcquireProgress = apt.progress.base.AcquireProgress
 
+        # Disable actual installation of downloaded items
+        self.cache.install_archives = (
+            lambda *a, **b: apt_pkg.PackageManager.RESULT_COMPLETED
+        )
+
     def tearDown(self):
         # this resets the rootdir apt_pkg.config to ensure it does not
         # "pollute" the later tests
@@ -62,11 +81,86 @@ class TestSignedUsable(testcommon.TestCase):
 
         apt.progress.text.AcquireProgress = self.progress
 
+    def doInstall(self, name, bargs):
+        self.cache[name].mark_install()
+        try:
+            with apt.progress.base.InstallProgress() as ip:
+                self.cache.commit(install_progress=ip, **bargs)
+        finally:
+            for fname in os.listdir(
+                os.path.join(self.chroot_path, "var/cache/apt/archives")
+            ):
+                if os.path.isfile(
+                    os.path.join(
+                        self.chroot_path, "var/cache/apt/archives", fname
+                    )
+                ):
+                    os.unlink(
+                        os.path.join(
+                            self.chroot_path, "var/cache/apt/archives", fname
+                        )
+                    )
+            self.cache[name].mark_keep()
+
+    def doFetchArchives(self, name, bargs):
+        fetcher = apt_pkg.Acquire()
+        self.cache[name].mark_install()
+        try:
+            self.cache.fetch_archives(fetcher=fetcher, **bargs)
+        finally:
+            for fname in os.listdir(
+                os.path.join(self.chroot_path, "var/cache/apt/archives")
+            ):
+                if fname.endswith(".deb"):
+                    os.unlink(
+                        os.path.join(
+                            self.chroot_path, "var/cache/apt/archives", fname
+                        )
+                    )
+            self.cache[name].mark_keep()
+
     def testDefaultDenyButExplicitAllowUnauthenticated(self):
         """Deny by config (default), but pass allow_unauthenticated=True"""
 
         bargs = dict(allow_unauthenticated=True)
         sargs = dict(allow_unauthenticated=True, unpack=False)
+
+        self.doInstall("signed-usable", bargs)
+        if RUN_CRASHING_TESTS:
+            self.assertRaisesRegex(
+                apt.cache.FetchFailedException,
+                CACHE_MSG_WEAK_HASH,
+                self.doInstall,
+                "signed-not-usable",
+                bargs,
+            )
+
+        self.doInstall("unsigned-usable", bargs)
+        if RUN_CRASHING_TESTS:
+            self.assertRaisesRegex(
+                apt.cache.FetchFailedException,
+                CACHE_MSG_WEAK_HASH,
+                self.doInstall,
+                "unsigned-unusable",
+                bargs,
+            )
+
+        self.doFetchArchives("signed-usable", bargs)
+        self.doFetchArchives("unsigned-usable", bargs)
+        self.assertRaisesRegex(
+            apt.cache.FetchFailedException,
+            CACHE_MSG_WEAK_HASH,
+            self.doFetchArchives,
+            "signed-not-usable",
+            bargs,
+        )
+        self.assertRaisesRegex(
+            apt.cache.FetchFailedException,
+            CACHE_MSG_WEAK_HASH,
+            self.doFetchArchives,
+            "unsigned-unusable",
+            bargs,
+        )
 
         self.cache["signed-usable"].candidate.fetch_binary(**bargs)
         self.cache["signed-usable"].candidate.fetch_source(**sargs)
@@ -84,6 +178,42 @@ class TestSignedUsable(testcommon.TestCase):
         bargs = dict()
         sargs = dict(unpack=False)
 
+        self.doInstall("signed-usable", bargs)
+        if RUN_CRASHING_TESTS:
+            self.assertRaisesRegex(
+                apt.cache.FetchFailedException,
+                CACHE_MSG_WEAK_HASH,
+                self.doInstall,
+                "signed-not-usable",
+                bargs,
+            )
+        self.doInstall("unsigned-usable", bargs)
+        if RUN_CRASHING_TESTS:
+            self.assertRaisesRegex(
+                apt.cache.FetchFailedException,
+                CACHE_MSG_WEAK_HASH,
+                self.doInstall,
+                "unsigned-unusable",
+                bargs,
+            )
+
+        self.doFetchArchives("signed-usable", bargs)
+        self.doFetchArchives("unsigned-usable", bargs)
+        self.assertRaisesRegex(
+            apt.cache.FetchFailedException,
+            CACHE_MSG_WEAK_HASH,
+            self.doFetchArchives,
+            "signed-not-usable",
+            bargs,
+        )
+        self.assertRaisesRegex(
+            apt.cache.FetchFailedException,
+            CACHE_MSG_WEAK_HASH,
+            self.doFetchArchives,
+            "unsigned-unusable",
+            bargs,
+        )
+
         self.cache["signed-usable"].candidate.fetch_binary(**bargs)
         self.cache["signed-usable"].candidate.fetch_source(**sargs)
         self.cache["signed-not-usable"].candidate.fetch_binary(**bargs)
@@ -95,6 +225,53 @@ class TestSignedUsable(testcommon.TestCase):
 
     def testDefaultDeny(self):
         """Test APT::Get::AllowUnauthenticated = False (default)"""
+        self.doInstall("signed-usable", {})
+        if RUN_CRASHING_TESTS:
+            self.assertRaisesRegex(
+                apt.cache.FetchFailedException,
+                CACHE_MSG_WEAK_HASH,
+                self.doInstall,
+                "signed-not-usable",
+                {},
+            )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doInstall,
+            "unsigned-usable",
+            {},
+        )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doInstall,
+            "unsigned-unusable",
+            {},
+        )
+
+        self.doFetchArchives("signed-usable", {})
+        self.assertRaisesRegex(
+            apt.cache.FetchFailedException,
+            CACHE_MSG_WEAK_HASH,
+            self.doFetchArchives,
+            "signed-not-usable",
+            {},
+        )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doFetchArchives,
+            "unsigned-usable",
+            {},
+        )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doFetchArchives,
+            "unsigned-unusable",
+            {},
+        )
+
         self.cache["signed-usable"].candidate.fetch_binary()
         self.cache["signed-usable"].candidate.fetch_source(unpack=False)
         self.assertRaisesRegex(
@@ -137,6 +314,53 @@ class TestSignedUsable(testcommon.TestCase):
 
         bargs = dict(allow_unauthenticated=False)
         sargs = dict(allow_unauthenticated=False, unpack=False)
+
+        self.doInstall("signed-usable", bargs)
+        if RUN_CRASHING_TESTS:
+            self.assertRaisesRegex(
+                apt.cache.FetchFailedException,
+                CACHE_MSG_WEAK_HASH,
+                self.doInstall,
+                "signed-not-usable",
+                bargs,
+            )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doInstall,
+            "unsigned-usable",
+            bargs,
+        )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doInstall,
+            "unsigned-unusable",
+            bargs,
+        )
+
+        self.doFetchArchives("signed-usable", bargs)
+        self.assertRaisesRegex(
+            apt.cache.FetchFailedException,
+            CACHE_MSG_WEAK_HASH,
+            self.doFetchArchives,
+            "signed-not-usable",
+            bargs,
+        )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doFetchArchives,
+            "unsigned-usable",
+            bargs,
+        )
+        self.assertRaisesRegex(
+            apt.cache.UntrustedException,
+            "Untrusted packages:",
+            self.doFetchArchives,
+            "unsigned-unusable",
+            bargs,
+        )
 
         self.cache["signed-usable"].candidate.fetch_binary(**bargs)
         self.cache["signed-usable"].candidate.fetch_source(**sargs)
